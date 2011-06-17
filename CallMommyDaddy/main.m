217d38476b9e624a10005456bb7c1911e4982f29
Marc Smith <marc.smith@gmail.com>
2011-06-17 18:42:38 -0400

    Initial Commit

---
+++ CallMommyDaddy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CallMommyDaddy
+//
+//  Created by Marc Smith on 6/17/11.
+//  Copyright NHL 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
